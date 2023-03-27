2504fa98f0e0ce1ebe18700d659c54e8bae31a27
“DenisReact” <den.lopatinski.dev@gmail.com>
2023-03-27 19:32:10 +0300

    feat(assets): added sidebar/header svg-icons

---
+++ icons/close-sidebar-icon.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+export default function CloseSidebarIcon() {
+	return (
+		<svg
+			xmlns='http://www.w3.org/2000/svg'
+			width='24'
+			height='24'
+			viewBox='0 0 24 24'
+			fill='none'
+		>
+			<path
+				d='M10 13H21M10 19H21'
+				stroke='#33355E'
+				stroke-width='2'
+				stroke-linecap='round'
+				stroke-linejoin='round'
+			/>
+			<path
+				d='M7 11L3 7M3 7L7 3M3 7L21 7'
+				stroke='#33355E'
+				stroke-width='2'
+				stroke-linecap='round'
+				stroke-linejoin='round'
+			/>
+		</svg>
+	);
+}
